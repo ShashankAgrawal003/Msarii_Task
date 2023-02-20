ea7c98d21eb44091609614b314ce84b9947b815f
Shashank Agrawal <shashank.agrawal_cs18@gla.ac.in>
2023-02-20 21:41:25 +0530

    Initial commit

---
+++ Task1/login.php
@@ -0,0 +1,60 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<?php
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  echo $email;
+  echo $password;
+  $db = new PDO('mysql:host=localhost;dbname=msarri_task_one', 'root', '');
+  $query = $db->prepare("SELECT * FROM msariiuser WHERE email = :email AND password = :password");
+  $query->bindParam(':email', $email);
+  $query->bindParam(':password', $password);
+  $query->execute();
+  $user = $query->fetch();
+  if ($user) {   
+    session_start();
+    $_SESSION['user_id'] = $user['id'];     // storing the id for future retrieval
+
+    // swal yaha ayega
+    ?>            
+    <script>
+      swal({                          
+      title: 'Logged In successfully!',
+      text: 'You will now be redirected to the home page.',
+      icon: 'success',
+      timer: 3000, 
+      showConfirmButton: false
+      });                
+      setTimeout(function() {
+      window.location.href = 'home.html';     // because previously header.location disturbing the work of swal ie immediate switch.
+      }, 3000);
+                
+    </script>
+
+
+    <?php
+    //header('Location:home.html');    // successFully logined then this page will open
+    exit();
+  } else {
+    echo $email;
+    echo $password;
+    echo "Invalid username or password.";
+    ?>            
+    <script>
+      swal({                          
+        title: 'Invalid username or password!',
+        text: 'You will now be redirected to the loginpage.',
+        icon: 'error',
+        timer: 3000, 
+        showConfirmButton: false
+      });                
+        setTimeout(function() {
+          window.location.href = 'login.html';     // because previously header.location disturbing the work of swal ie immediate switch.
+        }, 3000);
+                
+    </script>
+
+
+    <?php
+  }   
+?>
---
+++ Task1/register.php
@@ -0,0 +1,58 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<?php
+$DATABASE_HOST="localhost";
+$DATABASE_USER="root";
+$DATABASE_PASS="";
+$DATABASE_NAME="msarri_task_one";
+
+$con=mysqli_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME);
+if(mysqli_connect_error()){
+    exit('Error connection to the Database'.mysqli_connect_error());
+}
+
+if($stmt=$con->prepare('Select id, password from msariiuser where name=?')){
+    $stmt->bind_param('s',$_POST['name']);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows>0){
+        echo 'Username Already Exists. Try  Again';
+    }
+    else{
+        if($stmt=$con->prepare('insert into msariiuser(name,email,phoneNo,password) values (?,?,?,?)')){
+            $password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+            // $stmt->bind_param('ssis',$_POST['name'],$_POST['email'],$_POST['phoneNo'],$password);   // for encryting and storing on DB. But login password checking issue.
+            $stmt->bind_param('ssis',$_POST['name'],$_POST['email'],$_POST['phoneNo'],$_POST['password']);
+            $stmt->execute();
+            echo 'Successfully Registered';
+            ?>            
+            <script>
+                swal({                          
+                title: 'Registrated successfully / Data inserted successfully !',
+                text: 'You will now be redirected to the loginpage.',
+                icon: 'success',
+                timer: 3000, 
+                showConfirmButton: false
+            });                
+                setTimeout(function() {
+                window.location.href = 'login.html';     // because previously header.location disturbing the work of swal ie immediate switch.
+                }, 3000);
+                
+            </script>
+
+
+            <?php
+            //header('Location: login.html');
+        }
+        else{
+            echo 'Error Occurred';
+        }
+    }
+    $stmt->close();
+
+}
+else{
+    echo 'Error Occurred';
+}
+$con->close();
+?>
